6df38e946f039fb3a365aecb0a7b9255dd89cda1
apmaglobe <apmaglobe@gmail.com>
2026-09-16 05:24:11 +0400

    feat: provide installable PWA icons

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -11,8 +11,8 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#000000",
     lang: "az",
     icons: [
-      { src: "/icon", sizes: "192x192", type: "image/png", purpose: "any" },
-      { src: "/icon", sizes: "512x512", type: "image/png", purpose: "any" },
+      { src: "/icon-192", sizes: "192x192", type: "image/png", purpose: "any" },
+      { src: "/icon-512", sizes: "512x512", type: "image/png", purpose: "any" },
       { src: "/apple-icon", sizes: "180x180", type: "image/png", purpose: "any" },
     ],
   };
